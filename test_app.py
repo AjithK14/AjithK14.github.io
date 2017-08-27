342bf3aa3ec8c026d2511792ca1b183568c461e5
AjithK14 <kemisettia@gmail.com>
2017-08-27 19:05:09 -0400

    fixed favicon problems

--- test_app.py
+++ test_app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, request
 import os
+from flask import redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def student():
     return render_template('index.html')
+
+@app.route('/About.html')
+def about():
+    return render_template('About.html')
+
+@app.route("/favicon.ico")
+def favicon():
+    return(url_for('static',filename='favicon.ico'))
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
 @app.route('/send', methods = ['GET','POST'])
 def send():
     if request.method == 'POST':
